3719cc0a008cfd774f93c556639f0c47cf27eec4
bjergensen <bjergensen@unomaha.edu>
2025-02-02 21:28:49 +0000

    Finished

--- FutureTime.py
+++ FutureTime.py
@@ -9,21 +9,28 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
   #Ask user for minutes
-
+  hours_to_add = int(input("How many hours? "))
+  minutes_to_add = int(input("how many minutes? "))
+  
+  
   #Calculate the time after the user-supplied time has passed.
+  total_minutes = currentMinute + minutes_to_add
+  futureMins = total_minutes % 60
+  extraHour = (total_minutes // 60) + hours_to_add
+
+  futureHour = (currentHour + extraHour) % 24
 
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
+  print(f"Future time: {futureHour:02}:{futureMins:02}")
 
 if __name__ == '__main__':
   main()
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -8,11 +8,13 @@ import random
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+  print("Gag Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["yes", "No", "Maybe so", "I can't help you, I am a simple program"]
+  input("what is your question?")
   #Answer question randomly with one of the options from your earlier list.
-
+  response = random.choice(answers)
+  print(response)
 
 if __name__ == '__main__':
   main()
